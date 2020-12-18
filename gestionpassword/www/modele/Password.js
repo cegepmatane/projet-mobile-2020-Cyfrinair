93c5f62de82ae339b3574c85266071cca53ee3d4
Hyvong <Emustle@users.noreply.github.com>
2020-12-18 11:25:47 -0500

    Changement structure projet: gestionpassword -> cyfrinair
    Remplacement du projet gestionpassword pour cyfrinair (projet cordova incluant le répertoire platforms)

--- gestionpassword/www/modele/Password.js
+++
@@ -1,9 +0,0 @@
-class Password{
-    constructor(id, website, user, password, description){
-        this.id = id;
-        this.website = website;
-        this.user = user;
-        this.password = password;
-        this.description = description;
-    }
-}
