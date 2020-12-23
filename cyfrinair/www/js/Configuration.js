0ff1530eddcd578d143333d7d38617ef677f3be2
Mdaiglevigneault <44245132+Mdaiglevigneault@users.noreply.github.com>
2020-12-23 18:43:17 -0500

    build fonctionnel de l'appli

---
+++ cyfrinair/www/js/Configuration.js
@@ -0,0 +1,21 @@
+class Configuration
+{
+    static DUREE_ANIMATION = 1000;
+    static HAUTEUR_DONNEE = 100;
+    static MARGIN_DONNEE = 15;
+    static FONT = "24pt Arial";
+    static TAUX_RAFRAICHISSEMENT = 60;
+    static SCALE_CADENAS = 0.3;
+
+    //Alignement texte
+    static ALIGNEMENT_GAUCHE = "left";
+    static ALIGNEMENT_MILIEU = "center";
+    static ALIGNEMENT_DROITE = "right";
+
+    //Positionnement
+    static POS_GAUCHE = 0.05;
+    static POS_MILIEU_GAUCHE = 0.35;
+    static POS_MILIEU = 0.50;
+    static POS_MILIEU_DROITE = 0.65;
+    static POS_DROITE = 0.95;
+}
